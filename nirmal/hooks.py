2aab45a4b3e6f206339b74dbb4059e6c3b08266f
Harshit Jain <jnharshit1998@gmail.com>
2026-08-11 12:46:55 +0000

    customer alias in customer quick entry

--- nirmal/hooks.py
+++ nirmal/hooks.py
@@ -86,6 +86,12 @@ fixtures = [
         "dt": "Custom Field",
         "filters": [
             ["module", "in", ["Nirmal"]]
+        ],
+    },
+        {
+        "dt": "Property Setter",
+        "filters": [
+            ["module", "in", ["Nirmal"]]
         ]
     }
 ]
